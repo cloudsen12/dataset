8c029297674c1a535f79a5be68675b50a95688f4
fernando-desktop <fjprudenciop@gmail.com>
2021-01-05 12:52:29 -0500

    function to upload multiple files and their source folder

---
+++ src/upload_to_drive.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(googledrive)
+
+## upload folder and its contents
+drive_upload_full <- function(from, to) {
+
+  drive_mkdir(name = basename(from), path = to)
+
+  map(
+    list.files(from, full.names = T, recursive = F),
+    ~ drive_upload(
+      .x, verbose = FALSE,
+      path = sprintf("%1s/%2s/", to, basename(from))
+    )
+  )
+}
