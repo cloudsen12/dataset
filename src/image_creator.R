5caa399ced45e0f2905d3f2c7232544747f38aee
csaybar <csaybar@gmail.com>
2021-01-08 00:17:22 +0100

    working in clean dataset code

--- src/image_creator.R
+++ src/image_creator.R
@@ -1,7 +1,7 @@
-#' Create image in cloudsen12
+#' Script to create all the images in the cloudsen12 dataset
 #' @author csaybar
-#'
-#' Script used to manually select images in cloudsen12.
+
+# 1. Libraries
 library(googleCloudStorageR)
 library(googledrive)
 library(tidyverse)
@@ -17,38 +17,28 @@ library(rgee)
 library(png)
 library(sf)
 library(sp)
-
-set.seed(101)
 source("src/utils.R")
 
+# 2. Initialize Earth Engine
 ee_Initialize("aybar1994", drive = TRUE, gcs = TRUE)
 
-# 1. Load points with desired cloud average (after run point_creator.R)
+# 3. Load points with desired cloud average (after run point_creator.R)
 local_cloudsen2_points <- read_sf("data/cloudsen2_potential_points.geojson")
 
-# 2. Classify images in clear, almost clear, low-cloudy, mid-cloudy, cloudy
+# 4. Classify (label) images in clear, almost clear, low-cloudy, mid-cloudy, cloudy
 for (index in 1451:nrow(local_cloudsen2_points)) {
   cloudsen2_row <- local_cloudsen2_points[index,]
-  select_dataset_thumbnail_creator(
-    cloudsen2_row = cloudsen2_row,
-    n_images = "max",
-    kernel_size = c(255, 255),
-    data_range = c("2018-01-01", "2020-07-31"),
-    output = "results/"
-  )
+  select_dataset_thumbnail_creator(cloudsen2_row = cloudsen2_row)
 }
 
-# 3. Download images!
+# 5. List all the metadata
 jsonfiles <- list.files(
   path = "/home/csaybar/Documents/Github/cloudsen12/dataset/results/",
   pattern = "\\.json$",
   recursive = TRUE
 )
 
-for (index in seq_alon(jsonfiles)) {
-  dataset_creator_chips(
-    jsonfile = jsonfile,
-    kernel_size = c(255, 255),
-    output = "results/"
-  )
+# 6. Download all images in IRIS format :)
+for (jsonfile in seq_alon(jsonfiles)) {
+  dataset_creator_chips(jsonfile = jsonfile)
 }
--- src/utils.R
+++ src/utils.R
@@ -1,17 +1,25 @@
+#' CloudSEN12 thumbnail creator
+#'
+#' Create several thumbnail to find the clouds closest to the theoretical probabilities
+#' All Sentinel2 images have a Sentinel1 pair with no more than 2.5 days of delay.
+#'
 select_dataset_thumbnail_creator <- function(cloudsen2_row,
                                              n_images = 50,
                                              kernel_size = c(255, 255),
                                              data_range = c("2019-01-01", "2020-07-31"),
                                              output = "results/") {
-  dir.create("results",showWarnings = FALSE)
-  # 1. Create a point which represent the center of the chip
+  # 1. Create output directory
+  dir.create(output, showWarnings = FALSE)
+
+  # 2. Create a point which represent the center of the chip (from local to ee)
   point <- ee$Geometry$Point(cloudsen2_row$geometry[[1]])
 
-  # 2. Create a S2/S1 ImageCollection with all the necessary images.
+  # 3. Create a S2 ImageCollection.
   s2Sr <- ee$ImageCollection("COPERNICUS/S2_SR") %>%
     ee$ImageCollection$filterBounds(point) %>%
     ee$ImageCollection$filterDate(data_range[1], data_range[2])
 
+  # 4. Create a S1 ImageCollection.
   data_range2 <- c(as.Date(data_range[1]) - 5, as.Date(data_range[2]) + 5) %>% as.character()
   s1_grd <- ee$ImageCollection("COPERNICUS/S1_GRD") %>%
     ee$ImageCollection$filterBounds(point) %>%
@@ -22,33 +30,45 @@ select_dataset_thumbnail_creator <- function(cloudsen2_row,
     # Filter to get images collected in interferometric wide swath mode.
     ee$ImageCollection$filter(ee$Filter$eq("instrumentMode", "IW"))
 
+  # 5. Get dates from all the images which are part of S1 and S2 ImageCollection.
   s2_dates <- ee_get_date_ic(s2Sr) %>% as_tibble()
   s1_dates <- ee_get_date_ic(s1_grd) %>% as_tibble()
   sx_fx <- function(x) min(abs(s2_dates$time_start[x] - s1_dates$time_start))
   mindays <- sapply(seq_len(nrow(s2_dates)), sx_fx)
   valid_s2 <- s2_dates[mindays < 2.5,] # Pick up images with no more than 2.5 days of delay
-  message(sprintf("Number of images: %s", nrow(valid_s2)))
-  # 3. Create a S2 ImageCollection with all the necessary images.
-  img_crs <- s2Sr$first()$select(0)$projection()$getInfo()[["crs"]]
 
-  # 4. Donwload the images
-  images_position <- sample(nrow(valid_s2), nrow(valid_s2))
-  if (length(images_position) < 50) {
-    warning("Insufficient number of images ... PLEASE REPORT!")
+  # Display the number of available images with an S2/S1 pair
+  message(sprintf("Number of images: %s", nrow(valid_s2)))
+
+  if (n_images == "max") {
+    n_images <- nrow(valid_s2)
   }
 
-  # 4. Create folders to save results
+  # 6. Get the CRS of this specific point (5 images)
+  img_crs <- s2Sr$first()$select(0)$projection()$getInfo()[["crs"]]
+
+  # 7. shuffle valid images
+  images_position <- sample(nrow(valid_s2), nrow(valid_s2))
+
+  # if (length(images_position) < 50) {
+  #   warning("Insufficient number of images ... PLEASE REPORT!")
+  # }
+
+  # 8. Create a folder to save results
   dir_id <- sprintf("%s/point_%04d",output, cloudsen2_row$id)
   dir.create(dir_id, showWarnings = FALSE)
 
+  # 9. Download all the image thumbnails
   counter <- 0
   for (r_index in images_position) {
     if (counter == n_images) {
       break
     }
-    # 4.1 Select the image to download
+    # 9.1 Select the image
     img_to_download <- ee$Image(valid_s2$id[r_index])
     img_id <- basename(valid_s2$id[r_index])
+
+    # 9.2 Download the S2 thumbnail image (as a json)
     s2_img_array <- img_to_download %>%
       ee$Image$select(c("B4", "B3", "B2")) %>%
       ee$Image$addBands(ee$Image$pixelCoordinates(projection = img_crs)) %>%
@@ -60,14 +80,13 @@ select_dataset_thumbnail_creator <- function(cloudsen2_row,
                              projection = img_crs,
                              scale = 10) %>%
       ee$FeatureCollection$getInfo()
-    # Convert data from list to data_frame
 
-    # Sometimes points are not related to images .....
+
+    # Some image have FULL NA values if that occurs skip
     band_names <- try(
       expr = names(s2_img_array$features[[1]]$properties),
       silent = TRUE
     )
-
     if (class(band_names) == "try-error") {
       next
     }
@@ -76,22 +95,26 @@ select_dataset_thumbnail_creator <- function(cloudsen2_row,
       sprintf("Processing point [%s] image [%s] ... please wait",
               cloudsen2_row$id, counter)
     )
+
+    # 9.3 From list to data_frame
     extract_fn <- function(x) as.numeric(unlist(s2_img_array$features[[1]]$properties[x]))
     image_as_df <- do.call(cbind,lapply(band_names, extract_fn))
     colnames(image_as_df) <- band_names
     image_as_tibble <- as_tibble(image_as_df)
 
-    # If image have zero values next
+    # If all values in the image are zero, skip
     if (sum(image_as_tibble[["B2"]] == 0) > 0) {
       next
     } else {
       counter <- counter + 1
     }
 
+    # 9.4 From data_frame to sp; From sp to raster
     coordinates(image_as_tibble) <- ~x+y
     sf_to_stack <- function(x) rasterFromXYZ(image_as_tibble[x])
     final_stack <- stack(lapply(names(image_as_tibble), sf_to_stack))
 
+    # 9.4 Create a plot and download
     crs(final_stack) <- st_crs(img_crs)$proj4string
     png(sprintf("%s/%s.png", dir_id, img_id), 1000, 1000)
     max_value <- max(maxValue(final_stack))
@@ -99,43 +122,22 @@ select_dataset_thumbnail_creator <- function(cloudsen2_row,
     dev.off()
   }
 
-  # Apologize my messy code! :3
-  xy <- cloudsen2_row[["geometry"]][[1]] %>% as.numeric()
-  x <- xy[1]
-  y <- xy[2]
-  st_geometry(cloudsen2_row) <- NULL
-  list_w_data <- cloudsen2_row %>%
-    dplyr::select(starts_with("pcloud")) %>%
-    as.list()
-  names(list_w_data) <- sprintf("PUT_HERE_ID_%02d", 1:5)
-  list_w_data$x <- x
-  list_w_data$y <- y
-  list_w_data$comments <- "PUT_HERE_YOUR_COMMENT"
-  jsonlite::write_json(
-    x = list_w_data,
-    path = sprintf("%s/cprob_%04d.json", dir_id, cloudsen2_row$id),
-    pretty = TRUE,
-    auto_unbox = TRUE
-  )
+  # 10. Create the metadata.json file
   metadata_dataset_creator(
     cloudsen2_row = cloudsen2_row,
     output = output
   )
-  Sys.sleep(0.1)
-  file.remove(sprintf("%s/cprob_%04d.json", dir_id, cloudsen2_row$id))
 }
 
 
 metadata_dataset_creator <- function(cloudsen2_row,
                                      output = "results/") {
   dir_name_point <- sprintf("%s/point_%04d/", output, cloudsen2_row$id)
-  json_file <- sprintf("%s/cprob_%04d.json", dir_name_point, cloudsen2_row$id)
-  cprob <- jsonlite::read_json(json_file)
+  cloudsen2_row_no_sf <- st_drop_geometry(cloudsen2_row)
+  cprob_n <- names(cloudsen2_row_no_sf[4:8])
+  pprob_v <- as.numeric(cloudsen2_row_no_sf[4:8])
 
-  cprob_n <- names(cprob[1:5])
-  pprob_v <- as.numeric(cprob[1:5])
-
-  row_id <- gsub("cprob_|\\.json$", "", basename(json_file)) %>% as.numeric()
+  row_id <- cloudsen2_row$id
   metadata_point <- local_cloudsen2_points[row_id,]
   coord_xy <- as.numeric(metadata_point$geometry[[1]])
   param_id <- list()
@@ -151,7 +153,7 @@ metadata_dataset_creator <- function(cloudsen2_row,
   param_id$surface_type <- as.character(metadata_point$type)
   param_id$x <- coord_xy[1]
   param_id$y <- coord_xy[2]
-  param_id$comments <- cprob$comments
+  param_id$comments <- "PUT_HERE_YOUR_COMMENT"
   jsonlite::write_json(
     x = param_id,
     path = sprintf("%s/metadata_%04d.json", dir_name_point, cloudsen2_row$id),
@@ -160,10 +162,9 @@ metadata_dataset_creator <- function(cloudsen2_row,
   )
 }
 
-
 dataset_creator_chips <- function(jsonfile,
                                   kernel_size = c(255, 255),
-                                  output = "results/") {
+                                  output_final = "final_results/") {
   # 1. Read JSON file
   jsonfile_r <- jsonlite::read_json(jsonfile)
 
@@ -217,8 +218,7 @@ dataset_creator_chips <- function(jsonfile,
     crs(final_stack) <- st_crs(crs_kernel)$proj4string
 
     ### Prepare data for iris ------------------------
-    output_final <- "final_results/"
-    output_final_d <- "final_results/images"
+    output_final_d <- sprintf("%s/images", output_final)
     output_final_folder <- sprintf("%s/images/%s", output_final, basename(s2_id))
     metadata_final <- sprintf("%s/cloud-segmentation.json", output_final)
 
@@ -242,7 +242,6 @@ dataset_creator_chips <- function(jsonfile,
     input_data <- raster::stack(
       final_stack[[1:13]]/10000, final_stack[[14]], final_stack[[15:17]], final_stack[[22:23]]
     )
-
     # 18-19 -> cmask_s2cloudness| cmask_s2cloudness_reclass (0,1)
     # 20-21 -> cmask_sen2cor | cmask_sen2cor_reclass (0,1,2)
     benchmarch_data <- final_stack[[18:21]]
@@ -250,7 +249,6 @@ dataset_creator_chips <- function(jsonfile,
     writeRaster(x = benchmarch_data, filename = cloudmask_spec, overwrite = TRUE)
   }
 }
-
 
 ee_get_s1 <- function(point, s2_date, range = 60, exclude = NULL) {
   # 1. Defining range and ref kernel
