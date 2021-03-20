297e39d156409499d2af6722894d485eb45eb2fe
csaybar <csaybar@gmail.com>
2021-03-20 17:11:59 +0100

    add monitoring

--- test/monitoring.R
+++ test/monitoring.R
@@ -1,6 +1,11 @@
 library(ggplot2)
+library(lubridate)
 library(tidyverse)
 library(googledrive)
+library(googlesheets4)
+
+googledrive::drive_auth("s1078735@stud.sbg.ac.at")
+googlesheets4::gs4_auth("s1078735@stud.sbg.ac.at")
 
 # monitoring progress :3
 tile_monitoring <- function() {
@@ -17,12 +22,36 @@ tile_monitoring <- function() {
     sapply(function(x) x[[1]][[1]])
   time_img_selection <- time_img_selection[remove_cesar] %>% as.POSIXct %>% as.character
   img_sel_df <- data_frame(author = author_img_selection, date = as.Date(time_img_selection)) %>%
-    filter(date > as.Date("2021-02-12")) %>%
-    group_by(author) %>%
-    summarise(labels = length(author))
-  ggplot(img_sel_df, aes(x = author, y = labels)) +
+    group_by(author, cut(date, "week")) %>%
+    summarise(labels = length(author)) %>%
+    ungroup()
+  names(img_sel_df) <- c("author", "date", "nlabels")
+  ggplot(img_sel_df, aes(x = date, y = nlabels, group = author, color = author)) +
+      geom_line(size = 1.5) +
+      geom_point(size = 2.5) +
+      ylim(c(0, 300)) +
+      geom_hline(yintercept = 250, color = "red")
+}
+
+# hqlabels monitoring in progress :3
+hqlabels_monitoring <- function() {
+  xls <- read_sheet("https://docs.google.com/spreadsheets/d/1LpW9JY2BdhlQvAObD1BCzoBiliNRnU3fCRWMQJFRvoM")
+  xls_db <- xls %>%
+    filter(labeler  %in%  c("Jhomira", "Eduardo", "Fernando")) %>%
+    filter(!is.na(sen2_id))
+  txls <- table(xls_db[["labeler"]])
+  df_f <- data_frame(
+    labeler = names(txls),
+    points = floor(txls/5)
+  )
+
+  ggplot(df_f, aes(x = labeler, y = points)) +
     geom_bar(stat="identity") +
-    theme_classic()
+    theme_bw()
 }
 
 tile_monitoring()
+
+httr::set_config(httr::config( ssl_verifypeer = 0L))
+httr::set_config(httr::config(http_version = 0))
+hqlabels_monitoring()
